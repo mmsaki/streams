f599a049bd2cca107f5abb60ab7308e73c1d4de0
Meek <meek10x@gmail.com>
2023-06-07 02:18:38 -0500

    feat: object stream

---
+++ src/object-stream.js
@@ -0,0 +1,17 @@
+import through from 'through2';
+
+let size = 0;
+process.stdin.pipe(through.obj(write)).pipe(through.obj(write2, end));
+
+function write(buf, enc, next) {
+	next(null, { length: buf.length, total: (size += buf.length) });
+}
+
+function write2(obj, enc, next) {
+	console.log('obj=', obj);
+	next();
+}
+
+function end() {
+	console.log('size=', size);
+}
